c368d75b79ae041d5cd9b7b179bb8f725d06592d
d-laub <dlaub@ucsd.edu>
2022-11-22 09:36:59 -0800

    Refactor LES PyTorch to provide descriptor and distance functions.

--- les_pytorch/les.py
+++ les_pytorch/les.py
@@ -4,8 +4,8 @@ from torch import nn
 import einops as ein
 
 
-def _heat_kernel(x, sigma=2):
-    x = torch.as_tensor(x)
+def _heat_kernel(x: torch.Tensor, sigma=2):
+    """Heat kernel construction (normalized RBF kernel)."""
     # x: (... p m)
     w = torch.cdist(x, x) ** 2  # (... p p)
     # median: (... 1 1)
@@ -20,8 +20,8 @@ def _heat_kernel(x, sigma=2):
     return w
 
 
-def les_descriptor(w, mu=2.2e-16, m=2, nev=500, gamma=1e-6) -> torch.Tensor:
-    w = torch.as_tensor(w)
+def _log_eigenvalues(w: torch.Tensor, mu=2.2e-16, m=2, nev=500, gamma=1e-6) -> torch.Tensor:
+    """Approximate eigenvalue decomposition for PSD matrices (Tropp et al. 2017)"""
     # h: (... n n)
     b_dims = w.shape[:-2]
     n = w.shape[-2]
@@ -55,7 +55,51 @@ def les_descriptor(w, mu=2.2e-16, m=2, nev=500, gamma=1e-6) -> torch.Tensor:
     return torch.log(eigvals + gamma)
 
 
+def les_descriptor(x, sigma=2, mu=2.2e-16, m=2, nev=500, gamma=1e-6, kernel_fn=_heat_kernel) -> torch.Tensor:
+    """Compute the log-Euclidean descriptor(s) for a dataset.
+
+    Parameters
+    ----------
+    x : ArrayLike
+        a dataset or batch of datasets, last two dimensions correspond to obs x features
+    sigma : int, optional
+        by default 2
+    mu : float, optional
+        by default 2.2e-16
+    m : int, optional
+        by default 2
+    nev : int, optional
+        by default 500
+    gamma : float, optional
+        by default 1e-6
+    kernel_fn : Callable, optional
+        a function to 
+
+    Returns
+    -------
+    torch.Tensor
+        Log-Euclidean descriptor(s) for the input.
+    """
+    x = torch.as_tensor(x)
+    desc = _log_eigenvalues(kernel_fn(x, sigma), mu, m, nev, gamma)
+    return desc
+    
+
 def les_distance(desc1, desc2) -> torch.Tensor:
+    """Compute the log-Euclidean distance between two (batches of) log-Euclidean descriptors.
+
+    Parameters
+    ----------
+    desc1 : ArrayLike
+        First descriptor.
+    desc2 : ArrayLike
+        Second descriptor.
+
+    Returns
+    -------
+    torch.Tensor
+        Distance between descriptors.
+    """
     desc1 = torch.as_tensor(desc1)
     desc2 = torch.as_tensor(desc2)
 
@@ -78,20 +122,16 @@ class LES(nn.Module):
         self.gamma = gamma
         self._name = "LESLoss"
 
-    def forward(self, x1, x2):
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
         no_batch_dim = len(x1.shape) < 3
         if no_batch_dim:
             x1 = ein.rearrange(x1, "n p -> 1 n p")
             x2 = ein.rearrange(x2, "m o -> 1 m o")
 
         # (... n)
-        desc1 = les_descriptor(
-            _heat_kernel(x1, self.sigma), self.mu, self.m, self.nev, self.gamma
-        )
+        desc1 = les_descriptor(x1, self.sigma, self.mu, self.m, self.nev, self.gamma)
         # (... m)
-        desc2 = les_descriptor(
-            _heat_kernel(x2, self.sigma), self.mu, self.m, self.nev, self.gamma
-        )
+        desc2 = les_descriptor(x2, self.sigma, self.mu, self.m, self.nev, self.gamma)
         # (...)
         les_dist = les_distance(desc1, desc2)
 
