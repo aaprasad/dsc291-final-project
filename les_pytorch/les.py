764d5b66da68ff21c0cff59adcde2a1b9a6d8e65
d-laub <dlaub@ucsd.edu>
2022-11-22 09:06:16 -0800

    Refactor LES PyTorch to provide descriptor and distance functions.

--- les_pytorch/les.py
+++ les_pytorch/les.py
@@ -4,6 +4,70 @@ from torch import nn
 import einops as ein
 
 
+def _heat_kernel(x, sigma=2):
+    x = torch.as_tensor(x)
+    # x: (... p m)
+    w = torch.cdist(x, x) ** 2  # (... p p)
+    # median: (... 1 1)
+    median = ein.rearrange(w, "... p r -> ... 1 (p r)").quantile(0.5, -1, keepdim=True)
+    w = (-w / (sigma * median)).exp().clone()
+    d = 1 / w.sum(-1)  # (... p)
+    w *= d[..., None, :]  # (... 1 p)
+    w *= d[..., None]  # (... p 1)
+    d = 1 / w.sum(-1).sqrt()  # (... p)
+    w *= d[..., None, :]
+    w *= d[..., None]
+    return w
+
+
+def les_descriptor(w, mu=2.2e-16, m=2, nev=500, gamma=1e-6) -> torch.Tensor:
+    w = torch.as_tensor(w)
+    # h: (... n n)
+    b_dims = w.shape[:-2]
+    n = w.shape[-2]
+
+    with torch.no_grad():
+        # (... n m*nev)
+        omega: torch.Tensor
+        omega = torch.randn(*b_dims, n, m * nev, dtype=w.dtype)
+        omega, *_ = tla.qr(omega)
+
+    # (... n n) = (... n n) @ (... n n)
+    y = w @ omega
+    # (... n 1)
+    nu: torch.Tensor
+    nu = mu * tla.matrix_norm(y, ord=2, keepdim=True)
+
+    # (... n n) = (... n n) + (... 1 1) * (... n n)
+    y_nu = y + nu * omega
+    # (... n n) = (... n n) @ (... n n)
+    b = omega.transpose(-2, -1) @ y_nu
+    # (... n n)
+    c: torch.Tensor
+    c = tla.cholesky((b + b.transpose(-2, -1)) / 2).transpose(-2, -1)
+    # (... n)
+    eigvals: torch.Tensor
+    eigvals = tla.svdvals(y_nu @ tla.inv(c))
+    eigvals = torch.clamp(eigvals**2 - nu.squeeze(-1), min=0)
+    eigvals, _ = eigvals.sort()
+    eigvals = eigvals[..., -nev:]
+
+    return torch.log(eigvals + gamma)
+
+
+def les_distance(desc1, desc2) -> torch.Tensor:
+    desc1 = torch.as_tensor(desc1)
+    desc2 = torch.as_tensor(desc2)
+
+    smallest_n = min(desc1.shape[-1], desc2.shape[-1])
+    # (...)
+    les_dist = tla.vector_norm(
+        desc1[..., :smallest_n] - desc2[..., :smallest_n], dim=-1
+    )
+
+    return les_dist
+
+
 class LES(nn.Module):
     def __init__(self, sigma=2, mu=2.2e-16, m=2, nev=500, gamma=1e-6) -> None:
         super().__init__()
@@ -13,65 +77,25 @@ class LES(nn.Module):
         self.nev = nev
         self.gamma = gamma
         self._name = "LESLoss"
-    
-    def forward(self, x, y):
-        no_batch_dim = len(x.shape) < 3
+
+    def forward(self, x1, x2):
+        no_batch_dim = len(x1.shape) < 3
         if no_batch_dim:
-            x = ein.rearrange(x, 'n p -> 1 n p')
-            y = ein.rearrange(y, 'm o -> 1 m o')
-        
+            x1 = ein.rearrange(x1, "n p -> 1 n p")
+            x2 = ein.rearrange(x2, "m o -> 1 m o")
+
         # (... n)
-        x_eigvals = self._log_eigenvalues(self._heat_kernel(x))
+        desc1 = les_descriptor(
+            _heat_kernel(x1, self.sigma), self.mu, self.m, self.nev, self.gamma
+        )
         # (... m)
-        y_eigvals = self._log_eigenvalues(self._heat_kernel(y))
-        smallest_n = min(x_eigvals.shape[-1], y_eigvals.shape[-1])
+        desc2 = les_descriptor(
+            _heat_kernel(x2, self.sigma), self.mu, self.m, self.nev, self.gamma
+        )
         # (...)
-        les_dist = tla.vector_norm(x_eigvals[..., :smallest_n] - y_eigvals[..., :smallest_n], dim=-1)
-        
+        les_dist = les_distance(desc1, desc2)
+
         if no_batch_dim:
             les_dist.squeeze_()
-            
-        return les_dist
 
-    def _heat_kernel(self, x):
-        # x: (... p m)
-        w = torch.cdist(x, x)**2 # (... p p)
-        # median: (... 1 1)
-        median = ein.rearrange(w, '... p r -> ... 1 (p r)').quantile(0.5, -1, keepdim=True)
-        w = (- w / (self.sigma * median)).exp().clone()
-        d = 1 / w.sum(-1) # (... p)
-        w *= d[..., None, :] # (... 1 p)
-        w *= d[..., None] # (... p 1)
-        d = 1 / w.sum(-1).sqrt() # (... p)
-        w *= d[..., None, :]
-        w *= d[..., None]
-        return w
-    
-    def _log_eigenvalues(self, h):
-        # h: (... n n)
-        b_dims = h.shape[:-2]
-        n = h.shape[-2]
-        
-        with torch.no_grad():
-            # (b n m*nev)
-            omega = torch.randn(*b_dims, n, self.m*self.nev, dtype=h.dtype)
-            omega, *_ = tla.qr(omega)
-        
-        # (b n n) = (b n n) @ (b n n)
-        y = h @ omega
-        # (b n 1)
-        nu = self.mu * tla.matrix_norm(y, ord=2, keepdim=True)
-        
-        # (b n n) = (b n n) + (b 1 1) * (b n n)
-        y_nu = y + nu * omega
-        # (b n n) = (b n n) @ (b n n)
-        b = omega.transpose(-2, -1) @ y_nu
-        # (b n n)
-        c = tla.cholesky((b + b.transpose(-2, -1)) / 2).transpose(-2, -1)
-        # (b n)
-        eigvals = tla.svdvals(y_nu @ tla.inv(c))
-        eigvals = torch.clamp(eigvals**2 - nu.squeeze(-1), min=0)
-        eigvals, _ = eigvals.sort()
-        eigvals = eigvals[..., -self.nev:]
-        
-        return torch.log(eigvals + self.gamma)
+        return les_dist
